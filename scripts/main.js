4aed30b1bcd84861dbde3675357576a9e61a0f95
Nikolay Shutikov <nikolayshutikov@gmail.com>
2022-01-02 00:50:32 +0700

    markup on 320px

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,29 @@
 var swiper = new Swiper('.swiper', {
+  CSSWidthAndHeight: true,
+  slidesPerView:'auto',
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
     renderBullet: function (index, className) {
       return'<span class="' + className + '"></span>';
-    },
+    }    
   },
 });
+swiper.on('fromEdge', function (swiper) {
+  var brandItems=document.querySelectorAll(".brand-item");
+  if(brandItems && swiper.activeIndex!==0){
+    for(var i=0;i<brandItems.length;i++){
+      brandItems[i].style.removeProperty('margin-left');
+    }
+  }  
+});
+swiper.on('reachEnd', function () {
+  var brandItems=document.querySelectorAll(".brand-item");
+  if(brandItems){
+    for(var i=0;i<brandItems.length;i++){
+      brandItems[i].style.marginLeft="-65px";
+    }
+  }  
+});
 swiper = document.querySelector('.swiper').swiper;
+
