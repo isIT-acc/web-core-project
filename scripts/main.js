31c76d6983db933625186decad82527a92c268a5
Nikolay Shutikov <nikolayshutikov@gmail.com>
2022-01-12 03:41:33 +0700

    fixes, block 1.5

--- scripts/main.js
+++ scripts/main.js
@@ -1,38 +1,71 @@
-var hiddenTextLabel=document.querySelectorAll(".continuation__text");
+var hiddenTextLabels=document.querySelectorAll(".continuation__text");
 var swiperWrapper=document.querySelector(".swiper-wrapper");
+var brandItems=document.querySelectorAll(".brand-item");
+var showAllBtn=document.querySelector(".swiper__continuation");
 
-function addCheckboxClickListener(hiddenTextLabel){
-  var swiperShowAllBlock=hiddenTextLabel.parentElement;
-  var checkbox=hiddenTextLabel.children[0];
-  checkbox.addEventListener("change",function(){
-    if(this.checked){
-      hiddenTextLabel.style.transform="translateY(-50%) translateY(15px)";
-      if(swiperWrapper) swiperWrapper.style.height="auto";
-      this.nextElementSibling.nextSibling.textContent='Скрыть';
-      this.parentElement.parentElement.style.position="relative";
+if(showAllBtn && brandItems){
+  addOrRemoveTabindexAttr(brandItems,showAllBtn.getBoundingClientRect().top);
+}
+// add or remove tabindex attribute depends 
+// on relative position of topBorder of showAllBlock element
+function addOrRemoveTabindexAttr(elements,topBorder){
+  for (var i=brandItems.length-1;i>=0;i--){
+    if(brandItems[i].getBoundingClientRect().bottom > topBorder){
+      brandItems[i].removeAttribute("tabindex");
+      continue;
     }
-    else{
-      hiddenTextLabel.style.removeProperty("transform");
-      if(swiperWrapper) swiperWrapper.style.removeProperty("height");
-      this.nextElementSibling.nextSibling.textContent=this.parentElement.getAttribute("defaultContent");
-      this.parentElement.parentElement.style.removeProperty("position");
+    brandItems[i].setAttribute("tabindex","1");
+  }
+}
+
+
+
+function showAllBtnHandler(showAllBtn){
+  var classes=showAllBtn.classList;
+  if(classes.toggle("continuation__text--displayed")){
+    showAllBtn.style.transform="translateY(-50%) translateY(15px)";
+    if(swiperWrapper) swiperWrapper.style.height="auto";
+    showAllBtn.parentElement.style.position="relative";
+  }
+  else{
+    showAllBtn.style.removeProperty("transform");
+    if(swiperWrapper) swiperWrapper.style.removeProperty("height");
+    showAllBtn.parentElement.style.removeProperty("position");
+  }
+  if(showAllBtn && brandItems){
+    addOrRemoveTabindexAttr(brandItems,showAllBtn.getBoundingClientRect().top);
+  }
+}
+function addShowAllBtnClickListener(showAllBtn){
+  showAllBtn.addEventListener("click",function(){
+    showAllBtnHandler(this);
+  });
+}
+function addShowAllBtnKeyDownListener(showAllBtn){
+  showAllBtn.addEventListener("keydown",function(event){
+    if(event.code === "Space" || event.code === "Enter" ){
+      showAllBtnHandler(this);
     }
   });
 }
 
-if(hiddenTextLabel.length>0){
-  for(var i=0;i<hiddenTextLabel.length;i++){
-    addCheckboxClickListener(hiddenTextLabel[i]);
+if(hiddenTextLabels.length>0){
+  for(var i=0;i<hiddenTextLabels.length;i++){
+    addShowAllBtnClickListener(hiddenTextLabels[i]);
+    addShowAllBtnKeyDownListener(hiddenTextLabels[i]);
   }
 }
 
 window.addEventListener("resize",function(event){
+  if(showAllBtn && brandItems){
+    addOrRemoveTabindexAttr(brandItems,showAllBtn.getBoundingClientRect().top);
+  }
   if(window.innerWidth<768){
     if(swiperWrapper&&swiperWrapper.style.height) swiperWrapper.style.removeProperty("height");
   }
   else{ 
     if(swiperWrapper) swiperWrapper.removeAttribute("style");
-    if(swiperWrapper && !swiperWrapper.style.height && document.querySelector(".swiper__continuation .continuation__checkbox").checked){
+    if(swiperWrapper && !swiperWrapper.style.height && document.querySelector(".continuation__text--displayed")){
       swiperWrapper.style.height="auto";
     }
   }
